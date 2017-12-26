a443f6db8c2781186eb538f2f992b512f42c97ff
champlnwno <champgmonepiece@hotmail.com>
2017-12-26 20:32:30 +0700

    pkjj

--- Register.php
+++ Register.php
@@ -39,6 +39,7 @@ if (isset($_POST['submit'])) {
         <title>REGISTER</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="Styleregister.css">
+      
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
             crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
